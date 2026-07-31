c9c044f34272a5ce17b4ec8dee3347e5c633ea62
Dev-Caezar <okochristian6@gmail.com>
2026-07-31 21:34:57 +0100

    new

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import connectToDatabase from "./config/database.js";
 import app from "./app.js";
-
-dotenv.config({
-  path: "./.env",
-});
 
 const startServer = async () => {
   try {
